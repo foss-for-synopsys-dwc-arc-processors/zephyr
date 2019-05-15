2b4f221c8af37c7e909c07ff58b27513a7a17a36
Wayne Ren <wei.ren@synopsys.com>
2019-05-16 07:32:31 +0800

    board: qemu_archs: setup the correct mpu regions
    setup the correct mpu regions
    
    Signed-off-by: Wayne Ren <wei.ren@synopsys.com>

--- boards/arc/qemu_archs/arc_mpu_regions.c
+++ boards/arc/qemu_archs/arc_mpu_regions.c
@@ -10,25 +10,24 @@
 #include <linker/linker-defs.h>
 
 static struct arc_mpu_region mpu_regions[] = {
-        /* Region SRAM */
-        MPU_REGION_ENTRY("SRAM",
-                         DT_SRAM_BASE_ADDRESS,
-                         DT_SRAM_SIZE * 1024,
-                         /*REGION_KERNEL_ROM_ATTR | REGION_KERNEL_RAM_ATTR),*/
-                         REGION_ROM_ATTR | REGION_KERNEL_RAM_ATTR | REGION_DYNAMIC),
+	/* Region SRAM */
+	MPU_REGION_ENTRY("ROM",
+			 (u32_t)_image_rom_start,
+			 (u32_t)_image_rom_size,
+			 REGION_ROM_ATTR),
 
-        /*
-         * TODO: SRAM should be divided to ICCM and DCCM with 
-         * appropriate linker commands in place as well
-         */
-        /* Region Peripheral */
-        MPU_REGION_ENTRY("PERIPHERAL",
-                         DT_UART_NS16550_PORT_0_BASE_ADDR,
-                         DT_UART_NS16550_PORT_0_SIZE,
-                         REGION_KERNEL_RAM_ATTR),
+	MPU_REGION_ENTRY("RAM",
+			(u32_t)_image_ram_start,
+			256 * 1024,
+			REGION_KERNEL_RAM_ATTR),
+	/* Region Peripheral */
+	MPU_REGION_ENTRY("PERIPHERAL",
+			 DT_UART_NS16550_PORT_0_BASE_ADDR,
+			 DT_UART_NS16550_PORT_0_SIZE,
+			 REGION_KERNEL_RAM_ATTR),
 };
 
 struct arc_mpu_config mpu_config = {
-        .num_regions = ARRAY_SIZE(mpu_regions),
-        .mpu_regions = mpu_regions,
+	.num_regions = ARRAY_SIZE(mpu_regions),
+	.mpu_regions = mpu_regions,
 };
