766cbf5f9a5b53154fd4877a50d4c1d78ff65fcf
Alexey Brodkin <abrodkin@synopsys.com>
2019-09-03 15:42:23 +0300

    boards: arc: hsdk: Disable boot mirror on start
    BootROM is mapped to the beginning of address space
    on boot of the HSDK. So we obviously shouldn't use it
    for new code we load.
    
    Signed-off-by: Alexey Brodkin <abrodkin@synopsys.com>

--- soc/arc/snps_arc_hsdk/soc.c
+++ soc/arc/snps_arc_hsdk/soc.c
@@ -13,12 +13,30 @@
 #include <init.h>
 #include "soc.h"
 
+#define M_HS_CORE	0
+#define UPDATE_VAL	1
+
+#define ARC_PERIPHERAL_BASE	0xf0000000
+#define CREG_BASE		(ARC_PERIPHERAL_BASE + 0x1000)
+#define CREG_AXI_M_HS_CORE_BOOT	(CREG_BASE + 0x010)
+#define CREG_AXI_M_UPDT(m)	(CREG_BASE + 0x20 * (m) + 0x14)
+
 static int arc_hsdk_init(struct device *dev)
 {
 	ARG_UNUSED(dev);
 
 	u32_t core;
 	u32_t i;
+	u32_t reg;
+
+	/*
+	 * M_HS_CORE has one unique register - BOOT.
+	 * We need to clean boot mirror (BOOT[1:0]) bits in them to avoid first
+	 * aperture to be masked by 'boot mirror'.
+	 */
+	reg = *(u32_t *)CREG_AXI_M_HS_CORE_BOOT & (~0x3);
+	*(u32_t *)CREG_AXI_M_HS_CORE_BOOT = reg;
+	*(u32_t *)CREG_AXI_M_UPDT(M_HS_CORE) = UPDATE_VAL;
 
 	/* allocate all IDU interrupts to master core */
 	core = z_arc_v2_core_id();
