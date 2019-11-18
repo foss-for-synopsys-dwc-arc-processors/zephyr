88c02f777365f37db15391c42f7ba83373b37547
Wayne Ren <wei.ren@synopsys.com>
2019-11-19 07:16:37 +0800

    boards: qemu_archs: update the configuration
    * use the target simhs
    * update the configuration
    
    Signed-off-by: Wayne Ren <wei.ren@synopsys.com>

--- boards/arc/qemu_archs/board.cmake
+++ boards/arc/qemu_archs/board.cmake
@@ -3,13 +3,20 @@ set(EMU_PLATFORM qemu)
 set(QEMU_CPU_TYPE_${ARCH} arc)
 
 set(QEMU_FLAGS_${ARCH}
-  -M arc-sim
+  -M simhs
   -m 128M
   -nographic
   -no-reboot
   -monitor none
   -global cpu.firq=false
+  -global cpu.has-mpu=true
   -global cpu.mpu-numreg=16
+  -global cpu.num-irqlevels=15
+  -global cpu.num-irq=25
+  -global cpu.ext-irq=20
+  -global cpu.freq_hz=1000000
+  -global cpu.timer0=true
+  -global cpu.timer1=true
   )
 
 set(BOARD_DEBUG_RUNNER qemu)
