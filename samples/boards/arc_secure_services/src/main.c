8541e0ce759e4a529857c6f3928af9ed958770fe
Wayne Ren <wei.ren@synopsys.com>
2020-02-29 17:21:42 +0800

    samples: arc_secure_services: necessary changes after changes in secureshield
    necessary changes after changes in secureshield
    
    Signed-off-by: Wayne Ren <wei.ren@synopsys.com>

--- samples/boards/arc_secure_services/src/main.c
+++ samples/boards/arc_secure_services/src/main.c
@@ -29,7 +29,7 @@ void threadA(void *dummy1, void *dummy2, void *dummy3)
 
 	printk("Go to normal application\n");
 
-	arc_go_to_normal(*((uint32_t *)(NORMAL_FIRMWARE_ENTRY)));
+	z_arch_go_to_normal(*((uint32_t *)(NORMAL_FIRMWARE_ENTRY)));
 
 	printk("should not come here\n");
 
@@ -43,13 +43,6 @@ void main(void)
 {
 	/* necessary configuration before go to normal */
 	int32_t i = 0;
-
-	/* allocate timer 0 and timer1 to normal mode */
-	z_arc_v2_irq_uinit_secure_set(IRQ_TIMER0, 0);
-	z_arc_v2_irq_uinit_secure_set(IRQ_TIMER1, 0);
-
-	/* disable the secure interrupts for debug purpose*/
-	/* _arc_v2_irq_unit_int_disable(IRQ_S_TIMER0); */
 
 	while (1) {
 		printk("I am the %s thread in secure world: %d\n",
