e5a2e63f136108038b33ee6fac88469b92e0e3d4
Wayne Ren <wei.ren@synopsys.com>
2020-07-03 16:44:10 +0800

    arch: arc: don't use gcc's builtin to access aux regs
    * Metaware doesn't support gcc's builtin to access aux regs
    * use more generic inline assembly to access aux regs
    
    Signed-off-by: Wayne Ren <wei.ren@synopsys.com>

--- include/arch/arc/v2/aux_regs.h
+++ include/arch/arc/v2/aux_regs.h
@@ -160,20 +160,13 @@
 #define Z_ARC_V2_ECR_PARAMETER(X) (X & 0xff)
 
 #ifndef _ASMLANGUAGE
-#if defined(__GNUC__)
-
-#include <zephyr/types.h>
-#define z_arc_v2_aux_reg_read(reg) __builtin_arc_lr((volatile uint32_t)reg)
-#define z_arc_v2_aux_reg_write(reg, val) __builtin_arc_sr((unsigned int)val, (volatile uint32_t)reg)
-
-#else /* ! __GNUC__ */
 
 #define z_arc_v2_aux_reg_read(reg)                                \
 	({                                               \
 		unsigned int __ret;                      \
 		__asm__ __volatile__("       lr %0, [%1]" \
 				     : "=r"(__ret)       \
-				     : "i"(reg));        \
+				     : "ir"(reg));        \
 		__ret;                                   \
 	})
 
@@ -181,9 +174,8 @@
 	({                                                   \
 		__asm__ __volatile__("       sr %0, [%1]"    \
 				     :                       \
-				     : "ir"(val), "i"(reg)); \
+				     : "ir"(val), "ir"(reg)); \
 	})
-#endif /* __GNUC__ */
 
 #endif /* _ASMLANGUAGE */
 
