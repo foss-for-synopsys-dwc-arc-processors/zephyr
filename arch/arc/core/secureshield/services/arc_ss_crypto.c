667936ee71bfa31f551e411d822544cf7c23e838
Yuguo Zou <yuguo.zou@synopsys.com>
2020-12-11 15:08:10 +0800

    arch: arc: add ctr mode for arc secure crypto service
    arch: arc: add ctr_prng and ccm mode
    
    Signed-off-by: Yuguo Zou <yuguo.zou@synopsys.com>

--- arch/arc/core/secureshield/services/arc_ss_crypto.c
+++ arch/arc/core/secureshield/services/arc_ss_crypto.c
@@ -11,7 +11,10 @@
 #include "arch/arc/v2/secureshield/arc_ss_crypto.h"
 #include <tinycrypt/constants.h>
 #include <tinycrypt/aes.h>
+#include <tinycrypt/ctr_mode.h>
+#include <tinycrypt/ctr_prng.h>
 #include <tinycrypt/cbc_mode.h>
+#include <tinycrypt/ccm_mode.h>
 #include <tinycrypt/sha256.h>
 #include <tinycrypt/hmac.h>
 #include <tinycrypt/ecc.h>
@@ -47,6 +50,7 @@ uint32_t ss_crypto_tc_aes_encrypt(const uint8_t *key, const uint8_t *in,
 	uint32_t ret = TC_CRYPTO_FAIL;
 	struct tc_aes_key_sched_struct s;
 	//TODO: the key could be replaced by a key ID, leave the key to secure world could improve security
+	//TODO: aes set encrypt key and aes encrypt functions should be two seperate api, but need to protect the key_sched_struct first
 	ret = tc_aes128_set_encrypt_key(&s, key);
 	if(ret != TC_CRYPTO_SUCCESS)
 	{
@@ -80,8 +84,73 @@ uint32_t ss_crypto_tc_aes_decrypt(const uint8_t *key, const uint8_t *in,
 	return ret;
 }
 
+uint32_t ss_crypto_tc_aes_ctr_crypt(const uint8_t *key, ss_crypto_data_ptr in,
+										uint8_t *ctr, ss_crypto_data_ptr out)
+{
+	uint32_t ret = TC_CRYPTO_FAIL;
+	struct tc_aes_key_sched_struct s;
+	ret = tc_aes128_set_encrypt_key(&s, key);
+	if(ret != TC_CRYPTO_SUCCESS)
+	{
+		return ret;
+	}
+	ret = tc_ctr_mode(out->payload, out->size, in->payload, in->size, ctr, &s);
+	if(ret != TC_CRYPTO_SUCCESS)
+	{
+		return ret;
+	}
+	//TODO: ret value: TC_CRYPTO_SUCCESS=1, should have a unified success value for all secure servvices
+	return ret;
+
+}
+
+uint32_t ss_crypto_tc_ctr_prng_init(TCCtrPrng_t * const ctx,
+			ss_crypto_data_ptr entropy, ss_crypto_data_ptr personalization)
+{
+	uint32_t ret = TC_CRYPTO_FAIL;
+	ret = tc_ctr_prng_init(ctx, entropy->payload, entropy->size,
+					personalization->payload, personalization->size);
+	if(ret != TC_CRYPTO_SUCCESS)
+	{
+		return ret;
+	}
+	return ret;
+}
+
+uint32_t ss_crypto_tc_ctr_prng_reseed(TCCtrPrng_t * const ctx,
+			ss_crypto_data_ptr entropy, ss_crypto_data_ptr additional_input)
+{
+	uint32_t ret = TC_CRYPTO_FAIL;
+	ret = tc_ctr_prng_reseed(ctx, entropy->payload, entropy->size,
+					additional_input->payload, additional_input->size);
+	if(ret != TC_CRYPTO_SUCCESS)
+	{
+		return ret;
+	}
+	return ret;
+}
+
+uint32_t ss_crypto_tc_ctr_prng_generate(TCCtrPrng_t * const ctx,
+			ss_crypto_data_ptr additional_input, ss_crypto_data_ptr out)
+{
+	uint32_t ret = TC_CRYPTO_FAIL;
+	ret = tc_ctr_prng_generate(ctx, additional_input->payload,
+					additional_input->size, out->payload, out->size);
+	if(ret != TC_CRYPTO_SUCCESS)
+	{
+		return ret;
+	}
+	return ret;
+}
+
+uint32_t ss_crypto_tc_ctr_prng_uninstantiate(TCCtrPrng_t * const ctx)
+{
+	tc_ctr_prng_uninstantiate(ctx);
+	return TC_CRYPTO_SUCCESS;
+}
+
 uint32_t ss_crypto_tc_aes_cbc_encrypt(const uint8_t *key, ss_crypto_data_ptr in,
-											const uint8_t *iv, uint8_t *out)
+									const uint8_t *iv, ss_crypto_data_ptr out)
 {
 	uint32_t ret = TC_CRYPTO_FAIL;
 	struct tc_aes_key_sched_struct s;
@@ -91,7 +160,7 @@ uint32_t ss_crypto_tc_aes_cbc_encrypt(const uint8_t *key, ss_crypto_data_ptr in,
 	{
 		return ret;
 	}
-	ret = tc_cbc_mode_encrypt(out, in->size + TC_AES_BLOCK_SIZE, in->payload,
+	ret = tc_cbc_mode_encrypt(out->payload, out->size, in->payload,
 									in->size, iv, &s);
 	if(ret != TC_CRYPTO_SUCCESS)
 	{
@@ -102,7 +171,7 @@ uint32_t ss_crypto_tc_aes_cbc_encrypt(const uint8_t *key, ss_crypto_data_ptr in,
 }
 
 uint32_t ss_crypto_tc_aes_cbc_decrypt(const uint8_t *key, ss_crypto_data_ptr in,
-											const uint8_t *iv, uint8_t *out)
+									const uint8_t *iv, ss_crypto_data_ptr out)
 {
 	uint32_t ret = TC_CRYPTO_FAIL;
 	struct tc_aes_key_sched_struct s;
@@ -111,8 +180,8 @@ uint32_t ss_crypto_tc_aes_cbc_decrypt(const uint8_t *key, ss_crypto_data_ptr in,
 	{
 		return ret;
 	}
-	ret = tc_cbc_mode_decrypt(out, in->size + TC_AES_BLOCK_SIZE, in->payload,
-									in->size, iv, &s);
+	ret = tc_cbc_mode_decrypt(out->payload, out->size, in->payload, in->size,
+										iv, &s);
 	if(ret != TC_CRYPTO_SUCCESS)
 	{
 		return ret;
@@ -120,6 +189,54 @@ uint32_t ss_crypto_tc_aes_cbc_decrypt(const uint8_t *key, ss_crypto_data_ptr in,
 	//TODO: ret value: TC_CRYPTO_SUCCESS=1, should have a unified success value for all secure servvices
 	return ret;
 
+}
+
+uint32_t ss_crypto_tc_aes_ccm_config(TCCcmMode_t c, const uint8_t *key,
+						ss_crypto_data_ptr nonce, uint32_t mlen)
+{
+	uint32_t ret = TC_CRYPTO_FAIL;
+	struct tc_aes_key_sched_struct s;
+	ret = tc_aes128_set_decrypt_key(&s, key);
+	if(ret != TC_CRYPTO_SUCCESS)
+	{
+		return ret;
+	}
+	ret = tc_ccm_config(c, &s, nonce->payload, nonce->size, mlen);
+	if(ret != TC_CRYPTO_SUCCESS)
+	{
+		return ret;
+	}
+	return ret;
+}
+
+uint32_t ss_crypto_tc_aes_ccm_generation_encryption(ss_crypto_data_ptr out,
+				ss_crypto_data_ptr associated_data, ss_crypto_data_ptr payload,
+				TCCcmMode_t c)
+{
+	uint32_t ret = TC_CRYPTO_FAIL;
+	ret = tc_ccm_generation_encryption(out->payload, out->size,
+							associated_data->payload, associated_data->size,
+							payload->payload, payload->size, c);
+	if(ret != TC_CRYPTO_SUCCESS)
+	{
+		return ret;
+	}
+	return ret;
+}
+
+uint32_t ss_crypto_tc_aes_ccm_decryption_verification(ss_crypto_data_ptr out,
+				ss_crypto_data_ptr associated_data, ss_crypto_data_ptr payload,
+				TCCcmMode_t c)
+{
+	uint32_t ret = TC_CRYPTO_FAIL;
+	ret = tc_ccm_decryption_verification(out->payload, out->size,
+							associated_data->payload, associated_data->size,
+							payload->payload, payload->size, c);
+	if(ret != TC_CRYPTO_SUCCESS)
+	{
+		return ret;
+	}
+	return ret;
 }
 
 uint32_t ss_crypto_tc_sha256(ss_crypto_data_ptr data, uint8_t * digest)
@@ -238,13 +355,47 @@ uint32_t arc_s_service_crypto(uint32_t arg1, uint32_t arg2, uint32_t arg3,
 			return ss_crypto_tc_aes_decrypt((uint8_t *)arg1, (uint8_t *)arg2,
 											(uint8_t *)arg3);
 
+		case SS_TINYCRYPT_OP_AES_CTR_CRYPT:
+			return ss_crypto_tc_aes_ctr_crypt((uint8_t *)arg1,
+		(ss_crypto_data_ptr)arg2, (uint8_t *)arg3, (ss_crypto_data_ptr)arg4);
+
+		case SS_TINYCRYPT_OP_CTR_PRNG_INIT:
+			return ss_crypto_tc_ctr_prng_init((TCCtrPrng_t * const)arg1,
+						(ss_crypto_data_ptr)arg2, (ss_crypto_data_ptr)arg3);
+
+		case SS_TINYCRYPT_OP_CTR_PRNG_RESEED:
+			return ss_crypto_tc_ctr_prng_reseed((TCCtrPrng_t * const)arg1,
+						(ss_crypto_data_ptr)arg2, (ss_crypto_data_ptr)arg3);
+
+		case SS_TINYCRYPT_OP_CTR_PRNG_GEN:
+			return ss_crypto_tc_ctr_prng_generate((TCCtrPrng_t * const)arg1,
+						(ss_crypto_data_ptr)arg2, (ss_crypto_data_ptr)arg3);
+
+		case SS_TINYCRYPT_OP_CTR_PRNG_UNINST:
+			return ss_crypto_tc_ctr_prng_uninstantiate(
+												(TCCtrPrng_t * const)arg1);
+
 		case SS_TINYCRYPT_OP_AES_CBC_ENCRYPT:
 			return ss_crypto_tc_aes_cbc_encrypt((uint8_t *)arg1,
-				(ss_crypto_data_ptr)arg2, (uint8_t *)arg3, (uint8_t *)arg4);
+		(ss_crypto_data_ptr)arg2, (uint8_t *)arg3, (ss_crypto_data_ptr)arg4);
 
 		case SS_TINYCRYPT_OP_AES_CBC_DECRYPT:
 			return ss_crypto_tc_aes_cbc_decrypt((uint8_t *)arg1,
-				(ss_crypto_data_ptr)arg2, (uint8_t *)arg3, (uint8_t *)arg4);
+		(ss_crypto_data_ptr)arg2, (uint8_t *)arg3, (ss_crypto_data_ptr)arg4);
+
+		case SS_TINYCRYPT_OP_AES_CCM_CONFIG:
+			return ss_crypto_tc_aes_ccm_config((TCCcmMode_t)arg1,
+				(uint8_t *)arg2, (ss_crypto_data_ptr)arg3, arg4);
+
+		case SS_TINYCRYPT_OP_AES_CCM_GEN_ENCRYPT:
+			return ss_crypto_tc_aes_ccm_generation_encryption(
+				(ss_crypto_data_ptr)arg1, (ss_crypto_data_ptr)arg2,
+				(ss_crypto_data_ptr)arg3, (TCCcmMode_t)arg4);
+
+		case SS_TINYCRYPT_OP_AES_CCM_DECRYPT_VERF:
+			return ss_crypto_tc_aes_ccm_decryption_verification(
+				(ss_crypto_data_ptr)arg1, (ss_crypto_data_ptr)arg2,
+				(ss_crypto_data_ptr)arg3, (TCCcmMode_t)arg4);
 
 		case SS_TINYCRYPT_OP_SHA256:
 			return ss_crypto_tc_sha256((ss_crypto_data_ptr)arg1,
@@ -292,20 +443,79 @@ uint32_t ss_crypto_tc_aes_decrypt(const uint8_t *key, const uint8_t *in,
 					0, SS_TINYCRYPT_OP_AES_DECRYPT, 0, ARC_S_CALL_CRYPTO);
 }
 
+uint32_t ss_crypto_tc_aes_ctr_crypt(const uint8_t *key, ss_crypto_data_ptr in,
+										uint8_t *ctr, ss_crypto_data_ptr out)
+{
+	return z_arc_s_call_invoke6((uint32_t)key, (uint32_t)in, (uint32_t)ctr,
+		(uint32_t)out, SS_TINYCRYPT_OP_AES_CTR_CRYPT, 0, ARC_S_CALL_CRYPTO);
+}
+uint32_t ss_crypto_tc_ctr_prng_init(TCCtrPrng_t * const ctx,
+			ss_crypto_data_ptr entropy, ss_crypto_data_ptr personalization)
+{
+	return z_arc_s_call_invoke6((uint32_t)ctx, (uint32_t)entropy,
+			(uint32_t)personalization, 0, SS_TINYCRYPT_OP_CTR_PRNG_INIT, 0,
+			ARC_S_CALL_CRYPTO);
+}
+
+uint32_t ss_crypto_tc_ctr_prng_reseed(TCCtrPrng_t * const ctx,
+			ss_crypto_data_ptr entropy, ss_crypto_data_ptr additional_input)
+{
+	return z_arc_s_call_invoke6((uint32_t)ctx, (uint32_t)entropy,
+			(uint32_t)additional_input, 0, SS_TINYCRYPT_OP_CTR_PRNG_RESEED, 0,
+			ARC_S_CALL_CRYPTO);
+}
+
+uint32_t ss_crypto_tc_ctr_prng_generate(TCCtrPrng_t * const ctx,
+			ss_crypto_data_ptr additional_input, ss_crypto_data_ptr out)
+{
+	return z_arc_s_call_invoke6((uint32_t)ctx, (uint32_t)additional_input,
+		(uint32_t)out, 0, SS_TINYCRYPT_OP_CTR_PRNG_GEN, 0, ARC_S_CALL_CRYPTO);
+}
+
+uint32_t ss_crypto_tc_ctr_prng_uninstantiate(TCCtrPrng_t * const ctx)
+{
+	return z_arc_s_call_invoke6((uint32_t)ctx, 0, 0, 0,
+			SS_TINYCRYPT_OP_CTR_PRNG_UNINST, 0, ARC_S_CALL_CRYPTO);
+}
+
 uint32_t ss_crypto_tc_aes_cbc_encrypt(const uint8_t *key, ss_crypto_data_ptr in,
-											const uint8_t *iv, uint8_t *out)
+									const uint8_t *iv, ss_crypto_data_ptr out)
 {
 	return z_arc_s_call_invoke6((uint32_t)key, (uint32_t)in, (uint32_t)iv,
 		(uint32_t)out, SS_TINYCRYPT_OP_AES_CBC_ENCRYPT, 0, ARC_S_CALL_CRYPTO);
 }
 
 uint32_t ss_crypto_tc_aes_cbc_decrypt(const uint8_t *key, ss_crypto_data_ptr in,
-											const uint8_t *iv, uint8_t *out)
+									const uint8_t *iv, ss_crypto_data_ptr out)
 {
 	return z_arc_s_call_invoke6((uint32_t)key, (uint32_t)in, (uint32_t)iv,
 		(uint32_t)out, SS_TINYCRYPT_OP_AES_CBC_DECRYPT, 0, ARC_S_CALL_CRYPTO);
 }
 
+uint32_t ss_crypto_tc_aes_ccm_config(TCCcmMode_t c, const uint8_t *key,
+						ss_crypto_data_ptr nonce, unsigned int mlen)
+{
+	return z_arc_s_call_invoke6((uint32_t)c, (uint32_t)key, (uint32_t)nonce,
+				mlen, SS_TINYCRYPT_OP_AES_CCM_CONFIG, 0, ARC_S_CALL_CRYPTO);
+}
+
+uint32_t ss_crypto_tc_aes_ccm_generation_encryption(ss_crypto_data_ptr out,
+				ss_crypto_data_ptr associated_data, ss_crypto_data_ptr payload,
+				TCCcmMode_t c)
+{
+	return z_arc_s_call_invoke6((uint32_t)out, (uint32_t)associated_data,
+				(uint32_t)payload, (uint32_t)c,
+				SS_TINYCRYPT_OP_AES_CCM_GEN_ENCRYPT, 0, ARC_S_CALL_CRYPTO);
+}
+
+uint32_t ss_crypto_tc_aes_ccm_decryption_verification(ss_crypto_data_ptr out,
+				ss_crypto_data_ptr associated_data, ss_crypto_data_ptr payload,
+				TCCcmMode_t c)
+{
+	return z_arc_s_call_invoke6((uint32_t)out, (uint32_t)associated_data,
+				(uint32_t)payload, (uint32_t)c,
+				SS_TINYCRYPT_OP_AES_CCM_DECRYPT_VERF, 0, ARC_S_CALL_CRYPTO);
+}
 
 uint32_t ss_crypto_tc_sha256(ss_crypto_data_ptr data, uint8_t * digest)
 {
--- include/arch/arc/v2/secureshield/arc_ss_crypto.h
+++ include/arch/arc/v2/secureshield/arc_ss_crypto.h
@@ -7,18 +7,29 @@
 #ifndef ZEPHYR_INCLUDE_ARCH_ARC_V2_SS_CRYPTO_H_
 #define ZEPHYR_INCLUDE_ARCH_ARC_V2_SS_CRYPTO_H_
 
+#include <tinycrypt/aes.h>
 #include <tinycrypt/ecc.h>
+#include <tinycrypt/ctr_prng.h>
+#include <tinycrypt/ccm_mode.h>
 
-#define SS_TINYCRYPT_OP_AES_ENCRYPT  		0
-#define SS_TINYCRYPT_OP_AES_DECRYPT  		1
-#define SS_TINYCRYPT_OP_AES_CBC_ENCRYPT		2
-#define SS_TINYCRYPT_OP_AES_CBC_DECRYPT		3
-#define SS_TINYCRYPT_OP_SHA256       		4
-#define SS_TINYCRYPT_OP_HMAC         		5
-#define SS_TINYCRYPT_OP_ECC_SIGN			6
-#define SS_TINYCRYPT_OP_ECC_VERIFY			7
-#define SS_TINYCRYPT_OP_ECC_MAKE_KEY		8
-#define SS_TINYCRYPT_OP_ECC_SHARED_SECRET	9
+#define SS_TINYCRYPT_OP_AES_ENCRYPT  			0
+#define SS_TINYCRYPT_OP_AES_DECRYPT  			1
+#define SS_TINYCRYPT_OP_AES_CTR_CRYPT			2
+#define SS_TINYCRYPT_OP_CTR_PRNG_INIT			3
+#define SS_TINYCRYPT_OP_CTR_PRNG_RESEED			4
+#define SS_TINYCRYPT_OP_CTR_PRNG_GEN			5
+#define SS_TINYCRYPT_OP_CTR_PRNG_UNINST			6
+#define SS_TINYCRYPT_OP_AES_CBC_ENCRYPT			7
+#define SS_TINYCRYPT_OP_AES_CBC_DECRYPT			8
+#define SS_TINYCRYPT_OP_AES_CCM_CONFIG			9
+#define SS_TINYCRYPT_OP_AES_CCM_GEN_ENCRYPT		10
+#define SS_TINYCRYPT_OP_AES_CCM_DECRYPT_VERF	11
+#define SS_TINYCRYPT_OP_SHA256       			12
+#define SS_TINYCRYPT_OP_HMAC         			13
+#define SS_TINYCRYPT_OP_ECC_SIGN				14
+#define SS_TINYCRYPT_OP_ECC_VERIFY				15
+#define SS_TINYCRYPT_OP_ECC_MAKE_KEY			16
+#define SS_TINYCRYPT_OP_ECC_SHARED_SECRET		17
 //TODO: set RNG by call uECC_set_rng(), this is needed if platform does not own a PRNG
 
 
@@ -31,10 +42,27 @@ uint32_t ss_crypto_tc_aes_encrypt(const uint8_t *key, const uint8_t *in,
 										uint8_t *out);
 uint32_t ss_crypto_tc_aes_decrypt(const uint8_t *key, const uint8_t *in,
 										uint8_t *out);
+uint32_t ss_crypto_tc_aes_ctr_crypt(const uint8_t *key, ss_crypto_data_ptr in,
+										uint8_t *ctr, ss_crypto_data_ptr out);
+uint32_t ss_crypto_tc_ctr_prng_init(TCCtrPrng_t * const ctx,
+			ss_crypto_data_ptr entropy, ss_crypto_data_ptr personalization);
+uint32_t ss_crypto_tc_ctr_prng_reseed(TCCtrPrng_t * const ctx,
+			ss_crypto_data_ptr entropy, ss_crypto_data_ptr additional_input);
+uint32_t ss_crypto_tc_ctr_prng_generate(TCCtrPrng_t * const ctx,
+			ss_crypto_data_ptr additional_input, ss_crypto_data_ptr out);
+uint32_t ss_crypto_tc_ctr_prng_uninstantiate(TCCtrPrng_t * const ctx);
 uint32_t ss_crypto_tc_aes_cbc_encrypt(const uint8_t *key, ss_crypto_data_ptr in,
-											const uint8_t *iv, uint8_t *out);
+									const uint8_t *iv, ss_crypto_data_ptr out);
 uint32_t ss_crypto_tc_aes_cbc_decrypt(const uint8_t *key, ss_crypto_data_ptr in,
-											const uint8_t *iv, uint8_t *out);
+									const uint8_t *iv, ss_crypto_data_ptr out);
+uint32_t ss_crypto_tc_aes_ccm_config(TCCcmMode_t c, const uint8_t *key,
+						ss_crypto_data_ptr nonce, uint32_t mlen);
+uint32_t ss_crypto_tc_aes_ccm_generation_encryption(ss_crypto_data_ptr out,
+				ss_crypto_data_ptr associated_data, ss_crypto_data_ptr payload,
+				TCCcmMode_t c);
+uint32_t ss_crypto_tc_aes_ccm_decryption_verification(ss_crypto_data_ptr out,
+				ss_crypto_data_ptr associated_data, ss_crypto_data_ptr payload,
+				TCCcmMode_t c);
 uint32_t ss_crypto_tc_sha256(ss_crypto_data_ptr data, uint8_t * digest);
 uint32_t ss_crypto_tc_hmac(ss_crypto_data_ptr key, ss_crypto_data_ptr data,
 										uint8_t * digest);
