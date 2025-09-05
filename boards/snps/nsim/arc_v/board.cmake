f8fae171cf950375f70cf87d98dc295f09ac3f37
Afonso Oliveira <afonsoo@synopsys.com>
2025-09-05 12:54:09 +0100

    Add lldbac runner with board.json integration and multi-FPGA support
    - Implement lldbac runner for ARC architecture debugging and flashing
    - Support both hardware (FPGA) and simulator (nsim) modes with explicit selection
    - Add board.json configuration parsing for run-lldbac connection parameters
    - Support multiple FPGA programmers: djtgcfg and confprosh
    - Integrate with Zephyr west build system following runner standards
    - Add flash, debug, and debugserver command support

--- boards/snps/nsim/arc_v/board.cmake
+++ boards/snps/nsim/arc_v/board.cmake
@@ -8,9 +8,15 @@ string(REPLACE "/" "_" NSIM_BASE_FILENAME "${NSIM_BASE_FILENAME}")
 board_set_flasher_ifnset(arc-nsim)
 board_set_debugger_ifnset(arc-nsim)
 
+# Add lldbac runner support
+board_set_flasher_ifnset(lldbac)
+board_set_debugger_ifnset(lldbac)
+
 set(NSIM_PROPS "${NSIM_BASE_FILENAME}.props")
 board_runner_args(arc-nsim "--props=${NSIM_PROPS}")
+board_runner_args(lldbac "--nsim-props=${NSIM_PROPS}")
 
 board_finalize_runner_args(arc-nsim)
+board_finalize_runner_args(lldbac)
 include(${ZEPHYR_BASE}/boards/common/mdb-nsim.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/mdb-hw.board.cmake)
